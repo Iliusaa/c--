da59d6e1d58ffd6006b531f2cd07b4c1655edbd3
Shkip <iliushadd.md@gmail.com>
2025-12-05 16:01:38 +0200

    Commit

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+void A(){
+    cout<<"A";
+}
+void A1(){
+    cout<<"FUNC A1 CALLED";
+}
+void B(){
+    cout<<"FUNC B CALLED";
+}
+void C(){
+    cout<<"FUNC C CALLED";
+}
+
+
+
+
+
+int main(){
+    cout<< "Jello";
+A();
+
+
+
+    return 0;
+}
